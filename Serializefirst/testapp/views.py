62820ca07b39d913329eb1b125bf2ec7092fa46c
Punamchand <chavhanpunamchand@gmail.com>
2021-02-06 23:13:37 +0530

    deserialization_example_done

--- Serializefirst/testapp/views.py
+++ Serializefirst/testapp/views.py
@@ -1,28 +1,29 @@
-from django.shortcuts import render
-from django.http import HttpResponse,JsonResponse
+from django.http import HttpResponse, JsonResponse
 from rest_framework.renderers import JSONRenderer
+
 from .models import Student
 from .serializers import StudentSerializer
 
-def student_detail(request,pk):
-    stu=Student.objects.get(id=pk)
+
+def student_detail(request, pk):
+    stu = Student.objects.get(id=pk)
     print(stu)
-    serialized=StudentSerializer(stu)
+    serialized = StudentSerializer(stu)
     print(serialized)
     print(serialized.data)
 
-    json_data=JSONRenderer().render(serialized.data)
-    return HttpResponse(json_data,content_type='application/json')
+    json_data = JSONRenderer().render(serialized.data)
+    return HttpResponse(json_data, content_type='application/json')
     # return JsonResponse(serialized.data)
 
+
 def student_list(request):
-    stu=Student.objects.all()
+    stu = Student.objects.all()
     print(stu)
-    serialized=StudentSerializer(stu, many=True)
+    serialized = StudentSerializer(stu, many=True)
     print(serialized)
     print(serialized.data)
 
     # json_data=JSONRenderer().render(serialized.data)
     # return HttpResponse(json_data,content_type='application/json')
-    return JsonResponse(serialized.data,safe=False)
-
+    return JsonResponse(serialized.data, safe=False)
